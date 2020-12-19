3542e572b23377d77dec1ed6f3aff2e82b671417
Orion-News <56071046+Orion-News@users.noreply.github.com>
2020-12-19 13:14:20 -0300

    Update serve.js

--- serve.js
+++ serve.js
@@ -1,10 +1,1 @@
-const express = require('express');
-const routes = require('./src/routes');
 
-const app = express();
-app.use(express.json());
-app.use(routes);
-
-app.listen(3000, () => {
-	console.log(`subiu na porta ${3000}; para acessar click -> http://localhost:${3000}`, );
-});
