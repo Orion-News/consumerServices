89e36e92b67d1bbafb2c98ed3a6de1f79b98c65e
Orion-News <56071046+Orion-News@users.noreply.github.com>
2020-12-19 20:38:26 -0300

    Add files via upload

--- src/Controllers/coinController.js
+++ src/Controllers/coinController.js
@@ -1,10 +1,9 @@
 const coin = require('../Robots/coin.js');
 class Coin {
     async reponseOfCoin (request, response) {
-            const { searchBefore, searchAfter } = request.body.data;
-            const coins = await coin.CurrencyConverterTo( searchBefore, searchAfter );
-            response.send(`O valor para cada ${coins.searchCurrency}, ${ searchBefore } 
-                        é igual a ${ coins.CurrencyOfSearch }, ${ searchAfter }`);
+        const { searchBefore, searchAfter } = request.body.data;
+        const coins = await coin.CurrencyConverterTo( searchBefore, searchAfter );
+        response.send(coins);
     }
 }
 
