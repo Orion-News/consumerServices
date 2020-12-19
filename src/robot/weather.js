23e9d2e428f83132c51d3003c42f0d159c091b2f
Geverson <gsouza@3xsolutions.com.br>
2020-12-18 23:44:24 -0300

    wather: criado novo metodo para inteirar dentro dos elementos da DIV retornando os elementos filhos

--- src/robot/weather.js
+++ src/robot/weather.js
@@ -18,10 +18,22 @@ class RobotWeather {
                 tempWeather: tempWeather.textContent
             }
         });
-
         await browser.close();
 
         return searchWeather
+    }
+    async metodoTeste() {
+        const browser = await p.launch();
+        const page = await browser.newPage();
+        const url = `https://www.google.com/search?ei=gqLZX73EGN3C5OUPgPqAqAM&q=temperatura+em+manaus&oq=tempera`;
+        await page.goto(url);
+
+        const teste = await page.evaluate(() => {
+            const limp = document.querySelector('.mod').getElementsByTagName('span');
+                for(i=0; i< 11; i++ ) {
+                    console.log(limp[i].textContent)
+                }
+        })
     }
 }
 
