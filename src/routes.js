d43b2c67f9c192950e6a544fb4b9433ff03b1e04
Geverson <gsouza@3xsolutions.com.br>
2020-12-19 13:03:17 -0300

    routes: criado arquivo para separar as rotas do servidor

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const coinController = require('./Controllers/coinController.js');
+const weatherController = require('./Controllers/weatherController.js');
+
+const routes = express.Router();
+
+routes.get('/', (request, response) => {
+    return response.json({ Message : 'Hello Welcome, Choose a routes! ' })
+});
+
+// routes of robot coin
+routes.post('/coin', coinController.reponseOfCoin);
+
+
+// routes of robot weather
+routes.get('/weather', weatherController.responseWeatherSearchRobot);
+
+
+module.exports = routes;
