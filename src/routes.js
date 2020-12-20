0b10ba2d691fa0a6694628220ea9a69eb660ce27
Orion-News <56071046+Orion-News@users.noreply.github.com>
2020-12-20 15:55:55 -0300

    Add files via upload

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ const weatherController = require('./Controllers/weatherController.js');
 const routes = express.Router();
 
 routes.get('/', (request, response) => {
-    return response.json({ Message : 'Hello Welcome, Choose a routes! ' })
+    return response.json({ Message : 'Hello Welcome, Choose a routes! ' });
 });
 
 // routes of robot coin
