8cd0ac27ef84f29f06f562885f96e283d31f414a
Geverson <geeh-eb@hotmail.com>
2020-12-26 06:22:29 -0300

    routes: changed routes and create a new route

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const coinController = require('./Controllers/coinController.js');
 const weatherController = require('./Controllers/weatherController.js');
-
+const outlayController = require('./Controllers/outlayController.js');
 const routes = express.Router();
 
 routes.get('/', (request, response) => {
-    return response.json({ Message : 'Hello Welcome, Choose a routes! ' })
+    return response.json({ Message : 'Hello Welcome, Choose a routes! ' });
 });
 
 // routes of robot coin
-routes.post('/coin', coinController.reponseOfCoin);
+routes.post('/', coinController.reponseOfCoin);
 
 
 // routes of robot weather
-routes.get('/weather', weatherController.responseWeatherSearchRobot);
+routes.get('/', weatherController.responseWeatherSearchRobot);
 
+routes.post('/outlay', outlayController.store);
 
 module.exports = routes;
