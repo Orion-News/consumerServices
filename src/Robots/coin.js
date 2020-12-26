5eddd384aa9a87719e3f6ece6c07be2da2f9eff0
Geverson <geeh-eb@hotmail.com>
2020-12-26 06:20:59 -0300

    Coins-Robot: changed link of search currency

--- src/Robots/coin.js
+++ src/Robots/coin.js
@@ -5,10 +5,10 @@ class RobotCurrency {
 
     }
 
-    async CurrencyConverterTo(x,y) {
+    async CurrencyConverterTo(searchBefore,searchAfter) {
         const browser = await p.launch({ headless : true });
         const page = await browser.newPage();
-        const url = `https://www.google.com/search?q=${x}+para+${y}&oq=${x}+para+${y}&aqs=chrome..69i57j0i10i131i433j0i10l6.2706j1j7&sourceid=chrome&ie=UTF-8`;
+        const url = `https://www.google.com/search?q=${searchBefore}+para+${searchAfter}&oq=${searchBefore}+para+${searchAfter}&aqs=chrome..69i57j0i10i131i433j0i10l6.2706j1j7&sourceid=chrome&ie=UTF-8`;
         await page.goto(url);
         const currencyOfConverter = await page.evaluate(() => {
             return {
@@ -16,9 +16,14 @@ class RobotCurrency {
                 CurrencyOfSearch: document.querySelector('.a61j6.vk_gy.vk_sh.Hg3mWc').value
             }
         });
-        await browser.close();
+        
+        return this.formatedString(currencyOfConverter, searchBefore, searchAfter);
+        // await browser.close();
+    }
 
-        return currencyOfConverter;
+    formatedString (coins, searchBefore, searchAfter) {
+        return `O valor para cada ${coins.searchCurrency}, ${ searchBefore } 
+        é igual a ${ coins.CurrencyOfSearch }, ${ searchAfter }`
     }
     
 }
