b6ea50471eee9b7d21897354603606c029f52075
Geverson <geeh-eb@hotmail.com>
2020-12-26 06:20:00 -0300

    weather-Controller: changed class items

--- src/Controllers/weatherController.js
+++ src/Controllers/weatherController.js
@@ -1,12 +1,12 @@
-const weather = require('../Robots/weather');
+const weather = require('../Robots/weather.js');
 class Weather {
     constructor () {
 
     }
 
     async responseWeatherSearchRobot (req, res) {
-            const weather_ = await weather.weatherSearchRobot( req.body.local );
-            res.json(weather_);
+        const weather_ = await weather.weatherSearchRobot( req.body.local );
+        res.json(weather_);
     }
 }
 
