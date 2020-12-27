40662706cb4202c377bae7f98f89d86e026be5c4
Geverson <geeh-eb@hotmail.com>
2020-12-26 22:56:55 -0300

    outlay-Tests: created test to insert item in array kkkk

---
+++ src/Tests/outlayTest.js
@@ -0,0 +1,23 @@
+const { deepEqual, ok } = require('assert');
+const outlayService = require('../Services/outlayServices.js');
+
+const default_file_expected_register = {
+	"data" : {
+		"id" : "1",
+		"title" : "almoço",
+		"category": "food",
+		"spent": "30,00",
+		"date": "29/11/2020"
+	}
+}
+
+describe('pequenos testes para desembolsar', () => {
+    it('cadastrar um gasto', async () => {
+        const expectedItem = default_file_expected_register;
+
+        const insertItemExpected = await outlayService.store(default_file_expected_register.data);
+        const showExpectedItem = await outlayService.show(default_file_expected_register.id)
+
+        ok(insertItemExpected, showExpectedItem, expectedItem);
+    });
+});
