096e016adf27e0c4156df48fb15196c6e3e4c108
Geverson <geeh-eb@hotmail.com>
2020-12-26 22:55:59 -0300

    routes: created news routes to method of outlays

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ const weatherController = require('./Controllers/weatherController.js');
 const outlayController = require('./Controllers/outlayController.js');
 const routes = express.Router();
 
+// API HOME SERVER send from DOCs
 routes.get('/', (request, response) => {
     return response.json({ Message : 'Hello Welcome, Choose a routes! ' });
 });
@@ -15,7 +16,12 @@ routes.post('/', coinController.reponseOfCoin);
 // routes of robot weather
 routes.get('/', weatherController.responseWeatherSearchRobot);
 
+
+// routes from C.R.U.D Outlay;
 routes.post('/outlay', outlayController.store);
+routes.get('/outlay/:id', outlayController.show);
+routes.put('/outlay/:id', outlayController.update);
+routes.delete('/outlay/:id', outlayController.destroy);
 routes.get('/outlay', outlayController.index);
 
 module.exports = routes;
