64d3f65d26c2cfbd1df61858e2904baa05e876fc
Geverson <geeh-eb@hotmail.com>
2020-12-26 22:55:24 -0300

    outlay-Services: created news methods and comments of methods

--- src/Services/outlayServices.js
+++ src/Services/outlayServices.js
@@ -1,5 +1,6 @@
 const dataBase = [];
 
+// adicionar await nos metodos ao adicionar db;
 class outlayService {
     constructor () {
 
@@ -8,15 +9,30 @@ class outlayService {
     async store (data) {
         dataBase.push(data);
         const baseInsertItens = dataBase.find(x => x.id === data.id);
-        if (baseInsertItens) {
-            return `Registrado com sucesso`
-        } else {
-            return `Deu ruim`
-        }
+        return baseInsertItens ? `Registrado com sucesso` : `Deu ruim pra cadastrar`;
     }
 
     async index () {
-        return dataBase
+        return dataBase;
+    }
+
+    async show (id) {
+        const findItem = dataBase.find(x => x.id === id);
+        return findItem ? findItem : `Usuario não existe`;
+    }
+
+    async update (id, updateItem) {
+        const ItemIndex = dataBase.findIndex(x => x.id === id);
+        dataBase.splice(ItemIndex, 1);
+        dataBase.push(updateItem);
+        const updatedItem = dataBase.find(x => x.id === updateItem.id)
+        return updatedItem ? `Atualizado com sucesso` : `Deu ruim ao atualizar`;
+    }
+
+    async destroy (id) {
+        const destroys = dataBase.findIndex(x => x.id === id);
+        const del = dataBase.splice(destroys, 1);
+        return del ? `Item Deletado` : `berrou ao remover`
     }
 }
 
