832f1fee7a0736a8d35916000b345fd651ce3102
Geverson <geeh-eb@hotmail.com>
2020-12-26 22:54:06 -0300

    outlay-Controller: I modified some things so don't ask me what was kkkkkkk I commented before remembering

--- src/Controllers/outlayController.js
+++ src/Controllers/outlayController.js
@@ -1,3 +1,4 @@
+const { param } = require('../routes.js');
 const outlayServices = require('../Services/outlayServices.js');
 
 class outlayController {
@@ -6,13 +7,33 @@ class outlayController {
     }
 
     async store (request, response) {
-        const data = await outlayServices.store(request.body.data);
-        response.json(data)
+        const dataStore = await outlayServices.store(request.body.data);
+        response.json(dataStore);
     }
 
     async index (request, response) {
-        const data = await outlayServices.index();
-        response.json(data)
+        const indexData = await outlayServices.index();
+        response.json(indexData);
+    }
+
+    async show (request, response) {
+        const showData = await outlayServices.show(request.params.id);
+        response.json(showData);
+    }
+
+    async update (request, response) {
+        try {
+            const { data } = request.body;
+            const dataUpdate = await outlayServices.update(request.params.id, data);
+            response.json(dataUpdate);
+        } catch (e) {
+            throw new Error (e);
+        }
+    }
+
+    async destroy (request, response) {
+        const dataDestroy = await outlayServices.destroy(request.params.id);
+        response.json(dataDestroy);
     }
 }
 
