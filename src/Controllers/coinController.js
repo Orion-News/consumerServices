3921b4baed85b9fd54a4fb18182e3d96a1fdf51b
Geverson <geeh-eb@hotmail.com>
2020-12-28 22:03:27 -0300

    coin-Controller: changed name of variable coins to searchCurrency

--- src/Controllers/coinController.js
+++ src/Controllers/coinController.js
@@ -1,9 +1,9 @@
 const coin = require('../Robots/coin.js');
 class Coin {
     async reponseOfCoin (request, response) {
-        const { searchBefore, searchAfter } = request.body.data;
-        const coins = await coin.CurrencyConverterTo( searchBefore, searchAfter );
-        response.send(coins);
+        const { searchBefore, searchAfter, currency } = request.body.data;
+        const searchCurrency = await coin.CurrencyConverterTo( searchBefore, searchAfter, currency );
+        response.send(searchCurrency);
     }
 }
 
