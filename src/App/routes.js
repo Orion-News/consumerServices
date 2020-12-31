c693a95be52908e441d84872b600816ad4d38771
Geverson <geeh-eb@hotmail.com>
2020-12-31 19:35:21 -0300

    routes: changed file of local

---
+++ src/App/routes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const coinController = require('./Controllers/coinController.js');
+const weatherController = require('./Controllers/weatherController.js');
+const outlayController = require('./Controllers/outlayController.js');
+const userController = require('./Controllers/userController.js');
+const routes = express.Router();
+
+routes.use(express.json());
+
+// API HOME SERVER send from DOCs
+routes.get('/', (request, response) => {
+    return response.json({ Message : 'Hello Welcome, Choose a routes! ' });
+});
+
+// routes of robot coin
+routes.post('/coins', coinController.reponseOfCoin);
+
+// routes of robot weather
+routes.get('/weather', weatherController.responseWeatherSearchRobot);
+
+// routes from C.R.U.D User;
+reoutes.post('/register', userController.store);
+
+// routes from C.R.U.D Outlay;
+routes.post('/outlay', outlayController.store);
+routes.get('/outlay/:id', outlayController.show);
+routes.put('/outlay/:id', outlayController.update);
+routes.delete('/outlay/:id', outlayController.destroy);
+routes.get('/outlay', outlayController.index);
+
+module.exports = routes;
