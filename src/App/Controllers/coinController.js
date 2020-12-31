7f7ca38d13d6a9968e90c215622e51b064c6a7cf
Geverson <geeh-eb@hotmail.com>
2020-12-31 19:58:31 -0300

    coin-C: modified import of file required

--- src/App/Controllers/coinController.js
+++ src/App/Controllers/coinController.js
@@ -1,4 +1,4 @@
-const coin = require('../Robots/coin.js');
+const coin = require('../../Robots/coin.js');
 class Coin {
     async reponseOfCoin (request, response) {
         const { searchBefore, searchAfter, currency } = request.body.data;
