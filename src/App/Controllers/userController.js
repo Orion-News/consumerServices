39780d1b4567e4e737d0f9b23fca78d9e2fc6d7a
Geverson <geeh-eb@hotmail.com>
2021-01-01 12:23:52 -0300

    User-Controller: created methods of authenticate and show user

--- src/App/Controllers/userController.js
+++ src/App/Controllers/userController.js
@@ -2,16 +2,35 @@ const userServices = require('../Services/userServices');
 
 class UserController {
     
+    async Auth_ (request, response) {
+        const { email, password } = request.body.data;
+        try{
+            const user = await userServices.Auth_(email, password);
+            response.json(user);
+        } catch (e) {
+            return response.status(401).send({ "Unable to authenticate user" : `${e}`});
+        }
+    }
+
     async store (request, response) {
         const { data } = request.body;
         try {
             await userServices.verifyEmail(data.email);
             const userStore = await userServices.store(data);
-            response.json(userStore);
+            response.json({ "User registered successfully ": true, "User Name: " : userStore.name });
         } catch (e) {
-            return response.status(400).send({ error : `${e}`});
+            return response.status(400).send({ "Unable to register user" : `${e}`});
         }
     }
+
+    async show (request, response) {
+        try {
+            response.json(await userServices.show(request.params.id));
+        } catch (e) {
+            return response.status(404).send({ "Could not display user" : `${e}`});
+        }
+    }
+
 
 }
 
