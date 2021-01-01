cbb58e584715284486996ea193ee4a6571ff55e7
Geverson <geeh-eb@hotmail.com>
2021-01-01 01:22:32 -0300

    user-C: created verified in email before of save

--- src/App/Controllers/userController.js
+++ src/App/Controllers/userController.js
@@ -3,9 +3,16 @@ const userServices = require('../Services/userServices');
 class UserController {
     
     async store (request, response) {
-        const userStore = await userServices.store(request.body.data);
-        response.json(userStore);
+        const { data } = request.body;
+        try {
+            await userServices.verifyEmail(data.email);
+            const userStore = await userServices.store(data);
+            response.json(userStore);
+        } catch (e) {
+            return response.status(400).send({ error : `${e}`});
+        }
     }
+
 }
 
 module.exports = new UserController();
