9acaa0770abca7fcd700b759ee935b7809d0ce36
Geverson <geeh-eb@hotmail.com>
2020-12-31 21:09:38 -0300

    coin-C: added try and catch

--- src/App/Controllers/coinController.js
+++ src/App/Controllers/coinController.js
@@ -1,9 +1,14 @@
 const coin = require('../../Robots/coin.js');
+
 class Coin {
     async reponseOfCoin (request, response) {
-        const { searchBefore, searchAfter, currency } = request.body.data;
-        const searchCurrency = await coin.CurrencyConverterTo( searchBefore, searchAfter, currency );
-        response.send(searchCurrency);
+        try {
+            const { searchBefore, searchAfter, currency } = request.body.data;
+            const searchCurrency = await coin.CurrencyConverterTo( searchBefore, searchAfter, currency );
+            response.send(searchCurrency);
+        } catch (e) {
+            throw new Error(e);
+        }
     }
 }
 
