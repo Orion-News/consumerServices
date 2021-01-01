fb9788c44a1d88a1401b31bb7655aa2c8a375570
Geverson <geeh-eb@hotmail.com>
2021-01-01 01:28:40 -0300

    index: insert console to screen output when connected

--- src/App/Database/index.js
+++ src/App/Database/index.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/localDatabase', { useMongoClient: true });
+const url = 'mongodb://localhost:27017/LocalDataBase';
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true  });
 mongoose.Promise = global.Promise;
 
+const connection = mongoose.connection;
+connection.once("open", function() {
+  console.log("MongoDB database conectado com sucesso! na porta padrão", url);
+});
 module.exports = mongoose;
