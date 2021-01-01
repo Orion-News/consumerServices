d4efcfaf3f37ed98d863b040e6e14ac1184e986e
Geverson <geeh-eb@hotmail.com>
2020-12-31 21:11:24 -0300

    user-C: modified file to created method of store

--- src/App/Controllers/userController.js
+++ src/App/Controllers/userController.js
@@ -1,5 +1,11 @@
+const userServices = require('../Services/userServices');
+
 class UserController {
     
+    async store (request, response) {
+        const userStore = await userServices.store(request.body.data);
+        response.json(userStore);
+    }
 }
 
 module.exports = new UserController();
