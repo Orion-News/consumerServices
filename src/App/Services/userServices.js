bcca39d8204b5dbb8f366a3c0ac563a09f34f340
Geverson <geeh-eb@hotmail.com>
2021-01-01 01:29:18 -0300

    user-Services: created method to verify email before of save

--- src/App/Services/userServices.js
+++ src/App/Services/userServices.js
@@ -2,7 +2,13 @@ const User = require('../Database/Models/user.js');
 
 class userServices {
     async store (data) {
-        return await User.create(data);
+        const user = await User.create(data);
+        user.password = undefined;
+        return user
+    }
+
+    async verifyEmail(email) {
+        if (await User.findOne({ email })) throw new Error('Deu ruim, Email já cadastrado!');
     }
 }
 
