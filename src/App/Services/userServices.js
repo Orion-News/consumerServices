2cf9d576b140bb712f978d8cda078f6e1945043a
Geverson <geeh-eb@hotmail.com>
2021-01-01 12:24:27 -0300

    user-services: modified method of authenticate user

--- src/App/Services/userServices.js
+++ src/App/Services/userServices.js
@@ -1,5 +1,5 @@
 const User = require('../Database/Models/user.js');
-
+const bcrypt = require('bcryptjs');
 class userServices {
     async store (data) {
         const user = await User.create(data);
@@ -9,6 +9,22 @@ class userServices {
 
     async verifyEmail(email) {
         if (await User.findOne({ email })) throw new Error('Deu ruim, Email já cadastrado!');
+    }
+
+    async Auth_ (email, password) {
+        const searchUser =  await User.findOne({ email }).select('+password');
+
+        if (!searchUser || !searchUser.email) throw new Error(`User not found`);
+
+        if (!await bcrypt.compare(password, searchUser.password)) throw new Error(`Invalid Password`);
+
+        searchUser.password = undefined;
+
+        return searchUser;
+    }
+
+    async show (_id) {
+        return await User.findById({ _id });
     }
 }
 
