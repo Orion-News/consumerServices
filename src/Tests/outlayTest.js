127c53b5ce571bac4f0c84e934e403859e3adfb9
Geverson <geeh-eb@hotmail.com>
2021-01-05 00:52:13 -0300

    outlay-Test: create link of database and modified methods of file outlayServices to added connection with mongoDB

--- src/Tests/outlayTest.js
+++ src/Tests/outlayTest.js
@@ -12,12 +12,15 @@ const default_file_expected_register = {
 };
 
 const default_file_expected_updated = {
-    "data" : { 
-        "title" : "pneu",
-        "category" : "mecanic",
-        "spent" : "30,00",
-        "date" : "29/11/2020"
-    }
+    "data" : {
+        "_id": "5ff3d4cd80c85d15c01ff3d5",
+        "title": "almoço",
+        "category": "food",
+        "spent": 30,
+        "date": "29/11/2020",
+        "description": "bla bla bla blabla",
+        "__v": 0
+      }
 };
 
 describe('pequenos testes para desembolsar SERVICES', () => {
@@ -26,34 +29,33 @@ describe('pequenos testes para desembolsar SERVICES', () => {
         const expectedItem = default_file_expected_register;
 
         const insertItemExpected = await outlayService.store(expectedItem.data);
-        // const showExpectedItem = await outlayService.show(expectedItem._id);
+        const showExpectedItem = await outlayService.show(insertItemExpected._id);
 
-        ok(insertItemExpected, expectedItem.data);
+        ok(insertItemExpected, showExpectedItem, expectedItem.data);
     });
 
-    // it('deve listar um gasto', async () => {
-    //     const expected = default_file_expected_register.data;
+    it('deve listar um gasto', async () => {
+        const expected = default_file_expected_updated.data;
 
-    //     const showExpectedItem = await outlayService.show(expected.id);
+        const showExpectedItem = await outlayService.show('5ff3d4cd80c85d15c01ff3d5');
+        deepEqual( JSON.stringify(showExpectedItem),  JSON.stringify(expected));
+    });
 
-    //     deepEqual(showExpectedItem, expected);
-    // });
-
-    // it('deve atualizar um gasto pelo id', async () => {
-    //     const expected = default_file_expected_updated;
+    it('deve atualizar um gasto pelo id', async () => {
+        const expected = default_file_expected_updated.data;
     
-    //     const newObj = default_file_expected_updated;
+        const newObj = default_file_expected_updated.data;
     
-    //     await outlayService.update(expected.id, newObj);
-    //     const showExpectedItem = await outlayService.show(expected.id);
+        await outlayService.update(expected._id, newObj);
+        const showExpectedItem = await outlayService.show(expected._id);
     
-    //     deepEqual(showExpectedItem, expected);
-    // });
+        deepEqual(JSON.stringify(showExpectedItem), JSON.stringify(expected));
+    });
 
-    // it('deve deletar um gasto pelo id', async () => {
-    //     const expected = "Item Deletado";
-    //     const { data } = default_file_expected_register;
-    //     const deleteExpectedItem = await outlayService.destroy(data.id);
-    //     ok(deleteExpectedItem, expected);
-    // });
+    it('deve deletar um gasto pelo id', async () => {
+        const expected = "5ff3d6e6e12aed3770d4d5d4";
+        const { data } = default_file_expected_register;
+        const deleteExpectedItem = await outlayService.destroy(data._id);
+        ok(JSON.stringify(deleteExpectedItem), JSON.stringify(expected));
+    });
 });
