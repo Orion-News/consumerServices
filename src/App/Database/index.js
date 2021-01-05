4851858e67060bf3a1bd39a5207ba65b7e8d2697
Geverson <geeh-eb@hotmail.com>
2021-01-05 00:46:46 -0300

    index-Database: added attributes to stop alert notification

--- src/App/Database/index.js
+++ src/App/Database/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const url = 'mongodb://localhost:27017/LocalDataBase';
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true  });
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 mongoose.Promise = global.Promise;
 
 const connection = mongoose.connection;
