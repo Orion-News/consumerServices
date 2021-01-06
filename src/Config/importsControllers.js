928b27e120059c9fe83219ab329a857fa653fa39
Geverson <geeh-eb@hotmail.com>
2021-01-06 00:57:59 -0300

    imports-Controllers: created one file to imports all File-controllers

---
+++ src/Config/importsControllers.js
@@ -0,0 +1,13 @@
+const coinController = require('../App/Controllers/coinController.js');
+const weatherController = require('../App/Controllers/weatherController.js');
+const outlayController = require('../App/Controllers/outlayController.js');
+const userController = require('../App/Controllers/userController.js');
+const categoryController = require('../App/Controllers/categoryController.js');
+
+module.exports = {
+    coinController: coinController,
+    weatherController: weatherController,
+    outlayController: outlayController,
+    userController: userController,
+    categoryController: categoryController,
+};
