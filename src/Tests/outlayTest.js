8dbb6363407cb201261083834bc909f63d2ccb43
Geverson <geeh-eb@hotmail.com>
2021-01-06 01:14:00 -0300

    outlay-Test: modified method of imported file

--- src/Tests/outlayTest.js
+++ src/Tests/outlayTest.js
@@ -1,5 +1,5 @@
-const { deepEqual, ok } = require('assert');
-const outlayService = require('../App/Services/outlayServices.js');
+import { deepEqual, ok } from 'assert';
+import outlayService from '../App/Services/outlayServices.js';
 
 const default_file_expected_register = {
 	"data" : {
