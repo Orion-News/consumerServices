6a0f17ea6d3c5a76a1ac9304189ed1bd3091b217
Geverson <geeh-eb@hotmail.com>
2021-01-07 09:43:10 -0300

    importServices: create fil to import all services to app

---
+++ src/Config/importsServices.js
@@ -0,0 +1,9 @@
+import Auth_ from '../App/Services/authServices.js';
+import outlayServices from '../App/Services/outlayServices.js';
+import userServices from '../App/Services/userServices.js';
+
+module.exports = {
+    authServices: Auth_,
+    outlayServices: outlayServices,
+    userServices: userServices,
+}
