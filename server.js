d34975bca1a9567bf4de790ccc04786b5d8ac121
Geverson <geeh-eb@hotmail.com>
2021-01-06 22:46:12 -0300

    server: import new dependencie

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ class App {
 	constructor () {
 		this.express = express();
 		this.routes();
+		this.middlewares();
 	}
 
 	routes () {
 		this.express.use(routes);
 	}
 
+	middlewares () {
+		import 'dotenv/config';
+	}
 }
 
 module.exports = new App().express;
