effd6cdd5bef8188289036f244e61dedb5873a93
Geverson <geeh-eb@hotmail.com>
2021-01-07 00:09:52 -0300

    routes: created and import router of authenticate

--- src/App/routes.js
+++ src/App/routes.js
@@ -1,30 +1,37 @@
-import express from 'express';
+import { Router, json } from 'express';
 import { 
     coinController,
     userController,
     weatherController,
     outlayController, 
-    categoryController 
+    categoryController,
+    authController,
     } 
 from '../Config/importsControllers.js';
-const routes = express.Router();
 
-routes.use(express.json());
+import authMiddleware from '../App/middleware/auth.js';
+
+const routes = Router();
+
+routes.use(json());
 
 // API HOME SERVER send from DOCs
 routes.get('/', (request, response) => {
     return response.json({ Message : 'Hello Welcome, Choose a routes!' });
 });
 
-// routes of robot coin
-routes.post('/coins', coinController.reponseOfCoin);
-
-// routes of robot weather
-routes.get('/weather', weatherController.responseWeatherSearchRobot);
+// router to Categories;
+routes.get('/category', categoryController.index);
 
 // routes from register and login;
-routes.post('/login', userController.Auth_);
+routes.post('/login', authController.login);
 routes.post('/register', userController.store);
+
+routes.use(authMiddleware);
+
+
+// routes protected || routes protected \\
+routes.get('/authenticated', (req, res) => res.json({ Message : 'VERIFICAR TOKEN', status : true, UserId: req.userId }));
 
 // routes from C.R.U.D User;
 routes.get('/user/:id', userController.show);
@@ -37,7 +44,12 @@ routes.get('/outlay', outlayController.index);
 routes.get('/outlay/:id', outlayController.show);
 
 // router to Categories;
-routes.post('/category', categoryController.storeMany);
-routes.get('/category', categoryController.index);
+// routes.post('/category', categoryController.storeMany);
+
+// routes of robot coin
+routes.post('/coins', coinController.reponseOfCoin);
+
+// routes of robot weather
+routes.get('/weather', weatherController.responseWeatherSearchRobot);
 
 module.exports = routes;
